a4da1faa57f5ab814b6d6ac7ee0e2c697396aec9
Ahmed Modather <115614683+Ahmed-Modather@users.noreply.github.com>
2022-11-15 14:48:32 +0200

    Add a pop up  modal for setting the grid

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ const container = document.querySelector('.container');
       and its color become black when hovering.
     
 */
-function makeGrid(squaresPerRow) {
+function makeGrid(squaresPerRow = 24) {
 	let numberOfRows = squaresPerRow;
 
 	//create the rows first.
@@ -36,7 +36,7 @@ function makeGrid(squaresPerRow) {
 	}
 }
 
-makeGrid(24);
+makeGrid();
 setSquaresTime();
 
 // this function for each square's' mouse event "pointer go out from it" call timeout().
@@ -58,4 +58,41 @@ function timeout(target) {
 	timeId = setTimeout(() => {
 		target.style.background = 'white';
 	}, 5000);
+}
+
+/*  Function for a poping up a modal when the user press the button "setting the grid"  */
+function askUser() {
+	// Get the modal
+	const modal = document.querySelector('.modal');
+	modal.style.display = 'block';
+
+	// Get the <span> element that closes the modal
+	const close_btn = document.querySelector('.close-btn');
+	// Get the <button> element that set the grid and closes the modal
+	const ok_btn = document.querySelector('.ok-btn');
+
+	// When the user clicks on <span> (x), close the modal
+	close_btn.onclick = function () {
+		modal.style.display = 'none';
+	};
+
+	// When the user clicks anywhere outside of the modal, close it
+	window.onclick = function (event) {
+		if (event.target == modal) {
+			modal.style.display = 'none';
+		}
+	};
+
+	/* When the user input valed input (0 to 100) and press "OK" button
+		clear the old grid and creat new one with the new dimention */
+	ok_btn.addEventListener('click', () => {
+		const userInput = document.querySelector('#input').value;
+		console.log(userInput);
+		if (userInput >= 0 && userInput <= 100) {
+			container.innerHTML = '';
+			makeGrid(userInput);
+			setSquaresTime();
+			modal.style.display = 'none';
+		}
+	});
 }
